d741556a8dd4fd8e7ec06e98acb3cc6cbb774060
karimdsdm <karimdsdm@gmail.com>
2023-11-17 10:58:48 +0100

    Ajout de la pipeline

---
+++ tests/ActualitesTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Tests;
+
+use App\Entity\Actualites;
+use PHPUnit\Framework\TestCase;
+
+class ActualitesTest extends TestCase
+{
+    public function testSomething(): void
+    {
+        $this->assertTrue(true);
+
+        $actu = new Actualites();
+        
+    }
+}
